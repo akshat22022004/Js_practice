c54cc82cdbfa0406c9fde7f963855328003a8565
Akshat Singh <ayushsingh3393@gmail.com>
2025-06-26 01:52:33 +0530

    Making an ngnix server using node js

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 practicecodes
 # node js
 basic node js todo application.
+# Ngnix server
+Building of ngnix server using node js
---
+++ ngnix_project/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filepath = path.join(__dirname, req.url === '/' ? "index.html" : req.url);
+  const extname = String(path.extname(filepath).toLowerCase());
+
+  const mimetypes = {
+    ".html": "text/html",
+    ".js": "text/javascript",
+    ".css": "text/css",
+    ".png": "image/png",
+  };
+
+  const myContentType = mimetypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filepath, (err, content) => {
+    if (err) {
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.end("File not found");
+    } else {
+      res.writeHead(200, {"Content-Type": myContentType});
+      res.end(content, "utf-8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
