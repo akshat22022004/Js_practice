7d30b7b5a288f8e60097e3b43296d01e37dd03c7
Akshat Singh <ayushsingh3393@gmail.com>
2025-06-27 10:59:07 +0530

    Winston and Morgan Logger use and implementation.

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# Js_practice
+
+
+ # Js_practice
 practicecodes
 
+# Node JS
+Basic Node JS Todo Application.
 
-# node js
-basic node js todo application.
+This is a simple Todo application built using Node JS. It allows users to create, read, update and delete Todo items.
 
+ # Ngnix Server
+Building of Ngnix Server using Node JS
 
-# Ngnix server
-Building of ngnix server using node js
+This is a basic Ngnix server built using Node JS. It demonstrates how to create a server and handle HTTP requests.
 
-#Express operations 
+ # Express Operations
+Codes of how Express works and how we can perform the read, write, delete and update operations in it.
 
-Codes of how express works and how we can perform the read write delete and update operations in it.
+This section contains code examples that demonstrate how to perform CRUD (Create, Read, Update, Delete) operations using Express.
+
+ # Logging with Winston and Morgan
+Codes of how to use Winston and Morgan for logging in Node JS.
+
+This section contains code examples that demonstrate how to use Winston and Morgan for logging in Node JS.
+
+# Winston Logger
+Winston is a popular logging library for Node JS. It allows you to log messages with different levels of severity, such as info, warn, error, etc.
+
--- express/index.js
+++ express/index.js
@@ -1,10 +1,30 @@
 import 'dotenv/config';
 import express from "express";  
+import logger from "./logger.js";
+import morgan from "morgan"; 
 
 const app = express();
 const port = 8000 || process.env.PORT; // this is used run on 8000 or on the port 
-// any data which comes from front end is being parsed into  json using this line here.
+
 app.use(express.json());
+
+// How do you want your info comes to the console log in the terminal
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat, {
+    stream :{
+        write:(message) => {
+            const logObject = {
+                method: message.split(" ")[0],
+                url: message.split(" ")[1],
+                status: message.split(" ")[2],
+                responseTime: message.split(" ")[3]
+            };
+            logger.info(JSON.stringify(logObject));
+            console.log(JSON.stringify(logObject)); // added this line to print output in terminal
+        }
+    }
+}))
 
 let teaData = [];
 let nextid = 1;
@@ -12,6 +32,7 @@ let nextid = 1;
 // add a new tea
 // majourity of the time  you have to save data one uses post route
 app.post("/teas",(req,res) => {
+    console.log("POST");
     const {name,price} = req.body; // here desturturing is being done here
     const newtea = {
         id: nextid++,
@@ -20,11 +41,15 @@ app.post("/teas",(req,res) => {
     }
     teaData.push(newtea);
     res.status(201).json(newtea);
+    console.log(`Added new tea: ${JSON.stringify(newtea)}`); // added this line to print output in terminal
 })
+
 // get all teas
 app.get("/teas" , (req,res) => {
     res.status(200).send(teaData);
+    console.log(`Sent tea data: ${JSON.stringify(teaData)}`); // added this line to print output in terminal
 })
+
 // get tea with unique id
 // what ever comes after slash teas will be considered as id
 // if anything comes in the body we accept it as request.body.id 
@@ -36,15 +61,19 @@ app.get("/teas/:id" , (req,res) => { // get a tea with an respective id code
         return res.status(404).send("Tea not found");
     }
     res.status(200).send(tea);
+    console.log(`Sent tea: ${JSON.stringify(tea)}`); // added this line to print output in terminal
 })
+
 app.get("/" , (req,res) => {
     res.send("Hello World Chud gye guru");
 })
+
 app.get("/ice-tea" , (req,res) => {
     res.send("Thanks for ordering ice tea, its really hot");
 })
+
 // update  tea
-app.put("teas/:id" , (req,res) => {
+app.put("/teas/:id" , (req,res) => { // corrected the URL
     const tea = teaData.find(t => t.id == parseInt(req.params.id));
     if(!tea){
         return res.status(404).send("Tea not found");
@@ -53,10 +82,13 @@ app.put("teas/:id" , (req,res) => {
     tea.name = name;
     tea.price = price;
     res.status(200).send(tea);  
+    console.log(`Updated tea: ${JSON.stringify(tea)}`); // added this line to print output in terminal
 })
+
 app.listen(port, () => {
     console.log(`Server running at  ${port}`);
 }); 
+
 // delete tea
 app.delete("/teas/:id" , (req,res) => {
     const tea = teaData.find(t => t.id == parseInt(req.params.id));
@@ -66,6 +98,5 @@ app.delete("/teas/:id" , (req,res) => {
     // filtering we remove the tea which matches the id here.
     teaData = teaData.filter(t => t.id != parseInt(req.params.id));
     res.status(200).send("Tea deleted");
+    console.log(`Deleted tea: ${JSON.stringify(tea)}`); // added this line to print output in terminal
 });
-
-
